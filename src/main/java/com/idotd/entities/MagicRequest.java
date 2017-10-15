2f66b25df8e5e1144eb1e7ceb5c894edd1bcd995
Ardenian <22526933+Ardenian@users.noreply.github.com>
2017-10-15 19:19:55 +0200

    Added MagicRequest

---
+++ src/main/java/com/idotd/entities/MagicRequest.java
@@ -0,0 +1,15 @@
+package com.idotd.entities;
+
+public class MagicRequest {
+
+	public MagicRequest(int id, String proc) {
+		super();
+		this.id = id;
+		this.proc = proc;
+	}
+
+	private int id;
+
+	private String proc;
+	
+}
