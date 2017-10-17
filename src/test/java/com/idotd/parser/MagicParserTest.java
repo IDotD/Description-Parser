ad436523c5496a59ed930818379e2545fb3d768d
Björn Büttner <Idrinth@users.noreply.github.com>
2017-10-17 21:04:12 +0200

    adding test for most basic magic

--- src/test/java/com/idotd/parser/MagicParserTest.java
+++ src/test/java/com/idotd/parser/MagicParserTest.java
@@ -1,31 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.idotd.parser;
 
 import com.idotd.entities.Effect;
+import com.idotd.entities.MagicRequest;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MagicParserTest {
-    
-    public MagicParserTest() {
-    }
-
-    /**
-     * Test of parse method, of class MagicParser.
-     */
     @Test
     public void testParse() {
         System.out.println("parse");
-        MagicParser instance = null;
-        Effect expResult = null;
+        MagicParser instance = new MagicParser(new MagicRequest(17, "Each attack by any raid memBer has a 7.5% chance to deal 900.8% damage."));
         Effect result = instance.parse();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.print(result);
+        assertEquals(17, result.getId());
+        assertEquals(7.5, result.getChance());
+        assertEquals(900.8, result.getDamage());
+        assertTrue(result.getConditions().isEmpty());
     }
     
 }
