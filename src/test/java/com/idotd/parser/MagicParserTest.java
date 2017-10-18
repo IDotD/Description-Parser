58dadf634a58e0a120aca3d8146072cec29f1a04
Björn Büttner <Idrinth@users.noreply.github.com>
2017-10-18 21:11:30 +0200

    adding test for simple boss condition

--- src/test/java/com/idotd/parser/MagicParserTest.java
+++ src/test/java/com/idotd/parser/MagicParserTest.java
@@ -1,5 +1,6 @@
 package com.idotd.parser;
 
+import com.idotd.entities.BossTypeCondition;
 import com.idotd.entities.Effect;
 import com.idotd.entities.MagicRequest;
 import org.junit.Test;
@@ -16,6 +17,22 @@ public class MagicParserTest {
         assertEquals(7.5, result.getChance());
         assertEquals(900.8, result.getDamage());
         assertTrue(result.getConditions().isEmpty());
+    }
+    @Test
+    public void testParseWithBossCondition() {
+        System.out.println("Parse With Boss Condition");
+        MagicParser instance = new MagicParser(new MagicRequest(17, "Each attack by any raid memBer has a 7.5% chance to deal 900.8% damage; Extra 1,100% damage against Giant raids"));
+        Effect result = instance.parse();
+        System.out.print(result);
+        assertEquals(17, result.getId());
+        assertEquals(7.5, result.getChance());
+        assertEquals(900.8, result.getDamage());
+        assertFalse(result.getConditions().isEmpty());
+        assertEquals(1,result.getConditions().size());
+        BossTypeCondition firstResult = (BossTypeCondition) result.getConditions().get(0);
+        assertEquals(1100,firstResult.getDamage());
+        assertTrue(!firstResult.getTypes().isEmpty());
+        assertEquals("Giant",firstResult.getTypes().get(0));
     }
     
 }
